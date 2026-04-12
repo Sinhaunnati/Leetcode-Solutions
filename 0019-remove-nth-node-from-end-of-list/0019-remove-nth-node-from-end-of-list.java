0bb1e471df31ae3de054614e7fdb2180cc3d0a36
Sinhaunnati <unnatisinha132007@gmail.com>
2026-04-12 11:22:00 +0530

    Time: 0 ms (100%), Space: 43.7 MB (10.55%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -1,39 +1,28 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
- public class ListNode{
-struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
-    struct ListNode* temp=head;
-    int count=0;
-    while(temp!=NULL){
-        count++;
-        temp=temp->next;
-    }
-    if(n==count){
-          struct ListNode* newHead = head->next;
-        return newHead;
-    
-    }
-     temp = head;
-    for(int i = 1; i < count - n; i++){
-        temp = temp->next;
-    }
-    temp->next = temp->next->next;
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int count = 0;
+        ListNode temp = head;
 
-    return head;
+   
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+
+
+        if (n == count) {
+            return head.next;
+        }
+
+    
+        temp = head;
+        for (int i = 1; i < count - n; i++) {
+            temp = temp.next;
+        }
+
+     
+        temp.next = temp.next.next;
+
+        return head;
+    }
 }
- }
