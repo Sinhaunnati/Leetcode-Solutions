1691930dd46ac63c52fb94a009b4417b88af7a61
Sinhaunnati <unnatisinha132007@gmail.com>
2026-04-27 20:36:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,14 +1,39 @@
 class Solution {
     public void sortColors(int[] nums) {
-        for(int j=0;j<nums.length-1;j++){
-            for(int i=0;i<nums.length-1-j;i++){
-                if(nums[i]>nums[i+1]){
-                    int temp;
-                    temp=nums[i];
-                    nums[i]=nums[i+1];
-                    nums[i+1]=temp;
-                }
+        //Normal bubble sort 
+        // for(int j=0;j<nums.length-1;j++){
+        //     for(int i=0;i<nums.length-1-j;i++){
+        //         if(nums[i]>nums[i+1]){
+        //             int temp;
+        //             temp=nums[i];
+        //             nums[i]=nums[i+1];
+        //             nums[i+1]=temp;
+        //         }
+        //     }
+        // }
+
+
+        //National Dutch falg Algo:
+        int low=0;
+        int mid=0;
+        int high=nums.length-1;
+
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums,low++,mid++);
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums,mid,high--);
             }
         }
     }
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
 }
