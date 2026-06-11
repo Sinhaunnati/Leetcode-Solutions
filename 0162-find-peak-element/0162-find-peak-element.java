457126e8687f48226c89f485d632bdf22a253711
Sinhaunnati <unnatisinha132007@gmail.com>
2026-06-11 21:57:55 +0530

    Time: 0 ms (100%), Space: 44.1 MB (54.31%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,13 +1,33 @@
+// class Solution {
+//     public int findPeakElement(int[] nums) {
+//         if (nums.length == 1 || nums[0] > nums[1]) return 0;
+//         if (nums[nums.length-1] > nums[nums.length-2]) return nums.length - 1;
+//         for(int i=1;i<nums.length-1;i++){
+//             if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
+//                 return i;
+//             }
+//         }
+//         return -1;
+        
+//     }
+// }
+
+
+
 class Solution {
     public int findPeakElement(int[] nums) {
-        if (nums.length == 1 || nums[0] > nums[1]) return 0;
-        if (nums[nums.length-1] > nums[nums.length-2]) return nums.length - 1;
-        for(int i=1;i<nums.length-1;i++){
-            if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
-                return i;
+        int lo = 0, hi = nums.length - 1;
+
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                lo = mid + 1;  
+            } else {
+                hi = mid;      
             }
         }
-        return -1;
-        
+
+        return lo; 
     }
 }
