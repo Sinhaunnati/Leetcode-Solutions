fee35931398c5773ea7009fe7c936f214bc62ba1
Sinhaunnati <unnatisinha132007@gmail.com>
2026-06-19 19:59:19 +0530

    Time: 32 ms (17.04%), Space: 90.9 MB (19.82%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashMap<Integer,Integer> mpp=new HashMap<>();
+
+        for(int i=0;i<nums.length;i++){
+            mpp.put(nums[i],mpp.getOrDefault(nums[i],0)+1);
+            if(mpp.getOrDefault(nums[i],0)>1){
+                return nums[i];
+            }
+        }
+        return -1;
+        
+    }
+}
