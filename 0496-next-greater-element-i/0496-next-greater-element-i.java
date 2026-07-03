14c4e1c28b1c2f774dadcb63b03370a006fd85ea
Sinhaunnati <unnatisinha132007@gmail.com>
2026-07-03 18:52:10 +0530

    Time: 4 ms (74.74%), Space: 45.2 MB (61.58%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,27 +1,70 @@
+// class Solution {
+//     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+//         int[] ans = new int[nums1.length];
+//         for(int i=0;i<nums1.length;i++){
+//             int idx=-1;
+//             for(int j=0;j<nums2.length;j++){
+//                 if(nums1[i]==nums2[j]){
+//                     idx=j;
+//                     break;
+                    
+//                 }
+
+//             }
+//             int res=-1;
+//             for(int k=idx+1;k<nums2.length;k++){
+//                 if(nums2[k]>nums1[i]){
+//                     res=nums2[k];
+//                     break;
+//                 }
+//             }
+//             ans[i]=res;
+//         }
+//         return ans;
+        
+//     }
+// }
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        // Stack<Integer> st=new Stack<>();
-        int[] ans = new int[nums1.length];
-        for(int i=0;i<nums1.length;i++){
-            int idx=-1;
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                    idx=j;
-                    break;
-                    
-                }
+        HashMap<Integer,Integer> mpp=new HashMap<>();
+        Stack<Integer> st=new Stack<>();
+
+
+        for(int num:nums2){
+            while(!st.empty() && st.peek()<num){
+                mpp.put(st.pop(),num);
 
             }
-            int res=-1;
-            for(int k=idx+1;k<nums2.length;k++){
-                if(nums2[k]>nums1[i]){
-                    res=nums2[k];
-                    break;
-                }
-            }
-            ans[i]=res;
+            st.push(num);
+        }
+
+
+        int ans[]=new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = mpp.getOrDefault(nums1[i], -1);
         }
         return ans;
-        
+
+
+
     }
-}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
