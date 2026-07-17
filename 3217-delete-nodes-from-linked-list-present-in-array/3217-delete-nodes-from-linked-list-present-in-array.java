1f3d4eba51e14df1b5ba162e136dfcd4a5b4be20
Sinhaunnati <unnatisinha132007@gmail.com>
2026-07-17 19:39:11 +0530

    Time: 21 ms (89.33%), Space: 178.4 MB (36.26%) - LeetHub

---
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        HashSet<Integer> st=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            st.add(nums[i]);
+        }
+        ListNode dummy=new ListNode(-1);
+        dummy.next=head;
+        ListNode prev=dummy;
+        ListNode curr=head;
+        while(curr!=null){
+            if(st.contains(curr.val)){
+                prev.next=curr.next;
+            }
+            else{
+                prev=curr;
+            }
+            curr=curr.next;
+        }
+        return dummy.next;
+
+        
+    }
+}
