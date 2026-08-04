57df01e51be2901a77473b592cea4cb07cc343e1
Sinhaunnati <unnatisinha132007@gmail.com>
2026-08-04 21:59:50 +0530

    Time: 3 ms (81.76%), Space: 47 MB (42.16%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -12,20 +12,26 @@ class Solution {
                     }
             }
         
-        int[] freq=new int[max+1];
+        // int[] freq=new int[max+1];
 
-        for(int i=0;i<nums.length;i++){
-            freq[nums[i]]++;
-        }
+        // for(int i=0;i<nums.length;i++){
+        //     freq[nums[i]]++;
+        // }
         
-        ArrayList<Integer> res=new ArrayList<>();
-        for(int i=min;i<=max;i++){
-            if(freq[i]==0){
-                res.add(i);
+        // ArrayList<Integer> res=new ArrayList<>();
+        // for(int i=min;i<=max;i++){
+        //     if(freq[i]==0){
+        //         res.add(i);
 
-            }
+        //     }
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) set.add(n);
+        List<Integer> res = new ArrayList<>();
+        for (int i = min; i <= max; i++) {
+             if (!set.contains(i)) res.add(i);
+}
 
-         }
+         
          return res;
         
     }
