07177531db43b6537fa2040422ea534cc523313b
Sinhaunnati <unnatisinha132007@gmail.com>
2026-08-05 19:18:03 +0530

    Time: 9 ms (31.82%), Space: 46.9 MB (6.46%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,21 +1,44 @@
+
+// class Solution {
+//     public int[] smallerNumbersThanCurrent(int[] nums) {
+                // int[] ans=new int[nums.length];
+
+    //     for(int i=0;i<nums.length;i++){
+    //         int count=0;
+    //         for(int j=0;j<nums.length;j++){
+    //              if (nums[j] < nums[i]) {
+    //                 count++;
+    //             }
+    //         }
+
+    //             ans[i]=count;
+
+        
+        
+    // }
+    //     return ans;
+
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-                int[] ans=new int[nums.length];
 
-        for(int i=0;i<nums.length;i++){
-            int count=0;
-            for(int j=0;j<nums.length;j++){
-                 if (nums[j] < nums[i]) {
-                    count++;
-                }
+        int[] copy = nums.clone();     
+        Arrays.sort(copy);             
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < copy.length; i++) {
+            if (!map.containsKey(copy[i])) {
+                map.put(copy[i], i);
             }
+        }
 
-                ans[i]=count;
+        int[] ans = new int[nums.length];
 
-        
-        
-    }
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = map.get(nums[i]);
+        }
+
         return ans;
+    }
+}
 
-}
-}
