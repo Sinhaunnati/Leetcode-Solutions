c5113e7afddbedf8f9c2dba7d2cb6e0a14bb588d
Sinhaunnati <unnatisinha132007@gmail.com>
2026-08-22 18:46:25 +0530

    Time: 0 ms (100%), Space: 42.7 MB (78.37%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+
+        return Math.max(
+            robRange(nums, 0, n - 2),   // exclude last house
+            robRange(nums, 1, n - 1)    // exclude first house
+        );
+    }
+
+    private int robRange(int[] nums, int start, int end) {
+        int prev = 0, prev2 = 0;
+
+        for (int i = start; i <= end; i++) {
+            int pick = nums[i] + prev2;
+            int notPick = prev;
+            int cur = Math.max(pick, notPick);
+            prev2 = prev;
+            prev = cur;
+        }
+        return prev;
+    }
+}
