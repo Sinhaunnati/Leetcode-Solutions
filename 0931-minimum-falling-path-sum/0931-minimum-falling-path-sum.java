82d490b04a9800af4ebec9dfdc2cab6dbd4849f3
Sinhaunnati <unnatisinha132007@gmail.com>
2026-09-06 21:21:05 +0530

    Time: 7 ms (48.94%), Space: 47.8 MB (5.28%) - LeetHub

---
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int up = matrix[i - 1][j];
+                if (j > 0) {
+                    up = Math.min(up, matrix[i - 1][j - 1]);
+                }
+                if (j < n - 1) {
+                    up = Math.min(up, matrix[i - 1][j + 1]);
+                }
+                matrix[i][j] += up;
+            }
+        }
+
+        int minSum = Integer.MAX_VALUE;
+        for (int j = 0; j < n; j++) {
+            minSum = Math.min(minSum, matrix[n - 1][j]);
+        }
+        return minSum;
+    }
+}
