f80f3e37022e3853cdff7cd957c60b3187f7841a
Sinhaunnati <unnatisinha132007@gmail.com>
2026-09-07 17:53:08 +0530

    Time: 71 ms (47.46%), Space: 47.4 MB (99.29%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String removeStars(String s) {
+        Stack<Character> st=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(ch=='*'){
+                if(!st.empty()){
+                    st.pop();
+                }
+            }
+                else{
+                    st.push(ch);
+                }
+            }
+            StringBuilder result=new StringBuilder();
+            for(char ch:st){
+                result.append(ch);
+            }
+        
+        return result.toString();
+        
+    }
+}
