6e445aa0a6c25f88e528334ed932ee9d05ce2720
Sinhaunnati <unnatisinha132007@gmail.com>
2026-09-09 20:28:28 +0530

    Time: 7 ms (32.33%), Space: 44.4 MB (74.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,34 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if (s.length() != t.length()) return false;
+//         char[] ch1=s.toCharArray();
+//         Arrays.sort(ch1);
+//         char[] ch2=t.toCharArray();
+//         Arrays.sort(ch2);
+
+//         for(int i=0;i<ch1.length;i++){
+          
+//             if(ch1[i]!=ch2[i]) return false;
+//         }
+        
+//         return true;
+
+        
+//     }
+// }
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
-        char[] ch1=s.toCharArray();
-        Arrays.sort(ch1);
-        char[] ch2=t.toCharArray();
-        Arrays.sort(ch2);
 
-        for(int i=0;i<ch1.length;i++){
-          
-            if(ch1[i]!=ch2[i]) return false;
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
-        
-        return true;
 
-        
+        for (int c : count) {
+            if (c != 0) return false;
+        }
+        return true;
     }
 }
